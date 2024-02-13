00a049933dcb2e89fe776b201154acbff1937e26
brunolepis <brunka215@gmail.com>
2024-02-13 22:30:26 +0100

    remove unnecessary packages

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import "@fontsource-variable/inter";
-import "@fontsource-variable/lexend";
-import "@fontsource-variable/lexend-deca";
-import "@fontsource-variable/lexend-exa";
-import "@fontsource-variable/lexend-mega";
-import "@fontsource-variable/lexend-peta";
-import "@fontsource-variable/montserrat";
 import "@fontsource/krona-one";
 
 import { Navbar } from "./components/navbar.tsx";

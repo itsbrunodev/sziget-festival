9c6c52d23c9ce350314de622d576fa013b019e5a
brunolepis <brunka215@gmail.com>
2024-02-13 22:40:31 +0100

    remove focus styles from news email input

--- src/components/fresh-news.tsx
+++ src/components/fresh-news.tsx
@@ -12,11 +12,11 @@ export function FreshNews() {
           draggable={false}
         />
       </div>
-      <div className="flex w-full max-w-content sm:flex-row flex-col items-center justify-between gap-6 px-4 pt-12 sm:px-0">
+      <div className="flex w-full max-w-content flex-col items-center justify-between gap-6 px-4 pt-12 sm:flex-row sm:px-0">
         <h2 className="text-3xl font-black text-zinc-800">GET FRESH NEWS</h2>
         <div className="relative w-96">
           <input
-            className="full border-b-2 border-b-zinc-400 bg-transparent py-2 font-light text-zinc-700 outline-none transition-colors placeholder:text-zinc-400 focus:border-b-zinc-600"
+            className="full border-b-2 border-b-zinc-400 bg-transparent py-2 font-light text-zinc-700 outline-none transition-colors placeholder:text-zinc-400"
             type="text"
             placeholder="Enter your email to get fresh news"
           />
